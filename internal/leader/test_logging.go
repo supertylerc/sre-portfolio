40754a3ff9e89b921357d8814450d8e554914642
Jimmy Prescott <bunches_stands_0s@icloud.com>
2023-10-18 21:31:44 -0500

    update comment

--- internal/leader/test_logging.go
+++ internal/leader/test_logging.go
@@ -5,5 +5,5 @@ import "testing"
 
 func TestLogConfig(t *testing.T) {
 	// run LogConfig() with "APP_ENV" set to "production" and
-	// some other value as well to test handler options
+	// some other value as well to test handler log levels?
 }
