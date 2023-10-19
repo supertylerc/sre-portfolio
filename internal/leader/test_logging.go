227d156c9e1270ab9a63bd638d77bd4ae4b4d089
Jimmy Prescott <bunches_stands_0s@icloud.com>
2023-10-18 21:28:30 -0500

    logging.go test template

---
+++ internal/leader/test_logging.go
@@ -0,0 +1,9 @@
+package leader // is this correct?
+
+// import testing package
+import "testing"
+
+func TestLogConfig(t *testing.T) {
+	// run LogConfig() with "APP_ENV" set to "production" and
+	// some other value as well to test handler options
+}
