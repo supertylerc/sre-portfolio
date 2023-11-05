3cec86ddfbe0100bf3057d30285fcd1a5a4f2486
Jimmy Prescott <bunches_stands_0s@icloud.com>
2023-11-05 07:25:03 -0600

    working but not useful test

---
+++ pkg/leader/redis_test.go
@@ -0,0 +1,30 @@
+package leader
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/alicebob/miniredis/v2"
+	"github.com/redis/go-redis/v9"
+)
+
+func TestNewRedisLeader(t *testing.T) {
+
+	// start miniredis instance
+	mr := miniredis.RunT(t)
+	defer mr.Close()
+
+	// Create redis client
+	mrClient := redis.NewClient(&redis.Options{
+		Addr: mr.Addr(),
+	})
+
+	key := "leader"
+	ldr, err := NewRedisLeader(mrClient, key)
+	if err != nil {
+		fmt.Println("Failed to create leader")
+	}
+
+	// Print ldr object for now
+	fmt.Println(ldr)
+}
