ed2992f93c52c8fafc657064e7bdb3bc3fc59bf1
Jimmy Prescott <bunches_stands_0s@icloud.com>
2023-11-05 11:04:36 -0600

    fix all linter issues

--- pkg/leader/redis_test.go
+++ pkg/leader/redis_test.go
@@ -1,29 +1,30 @@
-package leader
+package leader_test
 
 import (
-	"fmt"
+	"log/slog"
 	"testing"
 
 	"github.com/alicebob/miniredis/v2"
 	"github.com/redis/go-redis/v9"
+	"github.com/supertylerc/scheduler/pkg/leader"
 )
 
 func TestNewRedisLeader(t *testing.T) {
-
+	t.Parallel()
 	// start miniredis instance
-	mr := miniredis.RunT(t)
+	mrInstance := miniredis.RunT(t)
+
+	key := "bGVhZGVyOnV1aWQ="
 
 	// Create redis client
 	mrClient := redis.NewClient(&redis.Options{
-		Addr: mr.Addr(),
+		Addr: mrInstance.Addr(),
 	})
 
-	key := "bGVhZGVyOnV1aWQ="
-	ldr, err := NewRedisLeader(mrClient, key)
+	ldr, err := leader.NewRedisLeader(mrClient, key)
 	if err != nil {
-		fmt.Println("Failed to create leader")
+		slog.Error("error creating UUID: %w", err)
 	}
 
-	// Print ldr object for now
-	fmt.Println(ldr)
+	slog.Debug("New Leader", "contains", ldr)
 }
