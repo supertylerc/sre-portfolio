5c147a325ac6e22ca5e2a5539c1c2b026f08c4eb
Jimmy Prescott <bunches_stands_0s@icloud.com>
2023-12-29 07:35:05 -0600

    struggle bus

--- pkg/leader/redis_test.go
+++ pkg/leader/redis_test.go
@@ -1,7 +1,6 @@
 package leader_test
 
 import (
-	"log/slog"
 	"testing"
 
 	"github.com/alicebob/miniredis/v2"
@@ -30,36 +29,33 @@ func TestNewRedisLeader(t *testing.T) {
 
 	miniServer.FastForward(leader.LeaderTTL)
 
+	ldr, _ := leader.NewRedisLeader(miniClient, "leader:uuid")
+
 	tests := []struct {
 		name  string
 		input string
+		ldr   *leader.RedisLeader
 		want  bool
 	}{
 		{
 			name:  "Become the leader",
 			input: "leader:uuid",
+			ldr:   ldr,
 			want:  true,
 		},
 		{
 			name:  "Not the leader",
 			input: "",
+			ldr:   ldr,
 			want:  true,
 		},
 	}
 
-	for _, testcase := range tests {
-		testcase := testcase
-		t.Run(testcase.name, func(t *testing.T) {
-			miniServer.FastForward(leader.LeaderTTL)
-			ldr, _ := leader.NewRedisLeader(miniClient, testcase.input)
-			slog.Info("New Leader", "contains", ldr.UUID)
-			_ = ldr.WriteLeader()
-			isLdr, _ := ldr.IsCurrentLeader()
-			slog.Info("Current Leader", "status", isLdr)
-			slog.Info("Current Leader", "UUID", ldr.UUID)
-
-			if isLdr != testcase.want {
-				t.Errorf("got %t; want %t", isLdr, testcase.want)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, _ := tt.ldr.IsCurrentLeader()
+			if got != tt.want {
+				t.Errorf("IsCurrentLeader() = %v, want %v", got, tt.want)
 			}
 		})
 	}
