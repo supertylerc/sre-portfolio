c5ee462604f6ac3edb47b130e48147381f173d41
Jimmy Prescott <bunches_stands_0s@icloud.com>
2023-12-29 07:17:17 -0600

    simplify table test

--- pkg/leader/redis_test.go
+++ pkg/leader/redis_test.go
@@ -1,6 +1,8 @@
 package leader_test
 
 import (
+	"fmt"
+	"log/slog"
 	"testing"
 
 	"github.com/alicebob/miniredis/v2"
@@ -11,49 +13,58 @@ import (
 func TestNewRedisLeader(t *testing.T) {
 	t.Parallel()
 
+	ctx := leader.Ctx
+
 	miniServer := miniredis.RunT(t)
-	t.Cleanup(func() { miniServer.Close() })
+	defer miniServer.Close()
+
+	// Set up miniredis client no leader
+	miniClient := redis.NewClient(&redis.Options{
+		Addr:       miniServer.Addr(),
+		ClientName: "testclient1",
+	})
+
+	err := miniClient.Set(ctx, "leader:uuid", "key", leader.LeaderTTL).Err()
+	if err != nil {
+		t.Errorf("Failed to set initial key/value")
+	}
+
+	fmt.Println(miniServer.Addr())
+
+	fmt.Println(miniClient)
+
+	miniServer.FastForward(leader.LeaderTTL)
 
 	tests := []struct {
-		name        string
-		input       string
-		want        string
-		miniClient  *redis.Client
-		miniOptions *redis.Options
-		leader      *leader.RedisLeader
-		ldrCheck    bool
+		name  string
+		input string
+		want  bool
 	}{
 		{
 			name:  "Become the leader",
 			input: "leader:uuid",
-			want:  "Becoming the leader.",
-			miniClient: redis.NewClient(&redis.Options{
-				Addr:       miniServer.Addr(),
-				ClientName: "testclient1",
-			}),
+			want:  true,
 		},
 		{
-			name:  "Become non-leader",
+			name:  "Not the leader",
 			input: "",
-			want:  "Becoming the leader.",
-			miniClient: redis.NewClient(&redis.Options{
-				Addr:       miniServer.Addr(),
-				ClientName: "testclient1",
-			}),
+			want:  false,
 		},
 	}
-
-	// miniServer.FastForward(leader.LeaderTTL)
 
 	for _, testcase := range tests {
 		testcase := testcase
 		t.Run(testcase.name, func(t *testing.T) {
-			t.Parallel()
-			ldr, _ := leader.NewRedisLeader(testcase.miniClient, testcase.input)
-			result, _ := ldr.IsCurrentLeader()
+			miniServer.FastForward(leader.LeaderTTL)
+			ldr, _ := leader.NewRedisLeader(miniClient, "leader:uuid")
+			fmt.Println("New Leader", "contains", ldr.UUID)
+			_ = ldr.WriteLeader()
+			isLdr, _ := ldr.IsCurrentLeader()
+			slog.Info("Current Leader", "status", isLdr)
+			slog.Info("Current Leader", "UUID", ldr.UUID)
 
-			if result != testcase.ldrCheck {
-				t.Errorf("NewRedisLeader() %v = %v, want %v", testcase.name, ldr, testcase.want)
+			if isLdr != testcase.want {
+				t.Errorf("got %t; want %t", isLdr, testcase.want)
 			}
 		})
 	}
